fafdd0289d50cd521460d3da23f714df5ffd4fd9
prettypinkanteater <audrh12@uci.edu>
2026-02-19 18:09:15 -0800

    Locator + Abstract Class Start

---
+++ GDIM 32 Final Project/Assets/Scripts/Item.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PickUp()
+    {
+
+    }
+
+    void PutDown()
+    {
+
+    }
+}
---
+++ GDIM 32 Final Project/Assets/Scripts/Locator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locator : MonoBehaviour
+{
+    public static Locator Instance { get; private set; }
+    public Player player { get; private set; }
+    // change player class name as needed, eg class is actually not named player lol
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+    }
+    void Start()
+    {
+       // once player object is created, find the gameobject via name and get its player component for the variable above.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
