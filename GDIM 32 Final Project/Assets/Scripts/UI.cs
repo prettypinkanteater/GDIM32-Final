cafdcd6e58ac283a8dd488a571606f6fee42f42d
Bjohnston07 <bwjohnst@uci.edu>
2026-02-22 14:02:49 -0800

    UI class

---
+++ GDIM 32 Final Project/Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject prompt;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    void showPrompt()
+    {
+        prompt.SetActive(true);
+    }
+}
