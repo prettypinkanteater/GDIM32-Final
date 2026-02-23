cb52a290f0a3177715b9e01aad118394a10e4ce4
prettypinkanteater <audrh12@uci.edu>
2026-02-22 20:34:18 -0800

    Knife Chop

--- GDIM 32 Final Project/Assets/Scripts/Items/Utensil.cs
+++ GDIM 32 Final Project/Assets/Scripts/Items/Utensil.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Utensil : Item
 {
+    bool chopable;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,20 @@ public class Utensil : Item
     // Update is called once per frame
     void Update()
     {
-        
+        if(chopable == true && Input.GetKey(KeyCode.Mouse0))
+        {
+            GetComponent<Animator>().SetTrigger("Chop");
+        }
+        else
+        {
+            GetComponent<Animator>().ResetTrigger("Chop");
+        }
+    }
+
+    protected override void PickUp()
+    {
+        base.PickUp();
+        transform.localRotation = Locator.Instance.player.transform.rotation;
+        chopable = true;
     }
 }
