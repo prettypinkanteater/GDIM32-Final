5c53dbaaa0aa76e75ba0cd0f799b099bad855d85
Bjohnston07 <bwjohnst@uci.edu>
2026-02-26 11:10:03 -0800

    Added GameController to Locator

--- GDIM 32 Final Project/Assets/Scripts/Locator.cs
+++ GDIM 32 Final Project/Assets/Scripts/Locator.cs
@@ -7,6 +7,8 @@ public class Locator : MonoBehaviour
     public static Locator Instance { get; private set; }
     public Player player { get; private set; }
     public UI ui { get; private set; }
+
+    public GameController gameController { get; private set; }
     // change player class name as needed, eg class is actually not named player lol
     private void Awake()
     {
@@ -22,6 +24,9 @@ public class Locator : MonoBehaviour
 
         GameObject uiGameObject = GameObject.Find("UI");
         ui = uiGameObject.GetComponent<UI>();
+
+        GameObject controllerObject = GameObject.Find("GameController");
+        gameController = controllerObject.GetComponent<GameController>();
 
     }
 
